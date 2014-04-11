5d9408a298aa2c0fd4417599026bb27c5e2ccc7e
Bai-Jie <BaiJie1991@gmail.com>
2014-04-11 23:16:35 +0800

    修正常量值Constant.url.PERSONAL_INFORMATION

--- src/main/java/org/orange/parser/parser/Constant.java
+++ src/main/java/org/orange/parser/parser/Constant.java
@@ -6,7 +6,7 @@ public final class Constant {
 		/** 取得教务处网站（含课程信息等）的会话cookie的页面 */
 		public static final String TEACHING_AFFAIRS_SESSION_PAGE = "http://ssfw.tjut.edu.cn/ssfw/j_spring_ids_security_check";
 		/** 师生服务网站“学生信息”中的“我的基本信息” */
-		public static final String PERSONAL_INFORMATION = "http://ssfw.tjut.edu.cn/ssfw/print/xjgl/jbxx.do";
+		public static final String PERSONAL_INFORMATION = "http://ssfw.tjut.edu.cn/ssfw/xjgl/jbxx.do";
 		public static final String LEARNING_COURSES = "http://ssfw.tjut.edu.cn/ssfw/xkgl/xkjgcx.do";
 		public static final String SELECTED_COURSES_FOR_NEXT_SEMESTER = "http://59.67.148.66/selection/bxqxkcx_z_new.asp";
 		public static final String FINAL_EXAM_GRADES = "http://59.67.148.66/score/xszxcjcy.asp";
